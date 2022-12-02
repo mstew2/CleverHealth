e10bd106ba2532ae289eb865270123bbe0688f1e
Nivig1310 <n.garimella@ufl.edu>
2022-12-02 17:24:04 -0500

    saved page

--- client/src/pages/Saved_Workouts.jsx
+++ client/src/pages/Saved_Workouts.jsx
@@ -1,79 +1,53 @@
 import React from "react";
+import {useeffect, useState} from "react";
 import { useEffect } from "react";
-import { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
-import axios from "axios";
+import Table from 'react-bootstrap/Table';
 
-const Home = () => {
-  const [posts, setPosts] = useState([]);
 
-  const cat = useLocation().search
+function SW(){
+  //array of workout objects
+  const [workouts, setWorkouts] = useState([{
+    name: '',
+    sets: '',
+    reps: ''
+  }])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(`/posts${cat}`);
-        setPosts(res.data);
-      } catch (err) {
-        console.log(err);
+  //fetches data from fetch link that is TBD
+  useEffect(()=>{
+    fetch("/<TBD>").then(res=>{
+      if(res.ok){
+        return res.json()
       }
-    };
-    fetchData();
-  }, [cat]);
-  // const posts = [
-  //   {
-  //     id: 1,
-  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-  //     img: "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-  //     img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-  //     img: "https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  //   },
-  //   {
-  //     id: 4,
-  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-  //     img: "https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  //   },
-  // ];
-
-  const getText = (html) =>{
-    const doc = new DOMParser().parseFromString(html, "text/html")
-    return doc.body.textContent
-  }
-
+    }).then(jsonRes=>setWorkouts(jsonRes));
+  })
 
   return (
-    <div className="home">
-      <div className="posts">
-        {posts.map((post) => (
-          <div className="post" key={post.id}>
-            <div className="img">
-              <img src={`../upload/${post.img}`} alt="" />
-            </div>
-            <div className="content">
-              <Link className="link" to={`/post/${post.id}`}>
-                <h1>{post.title}</h1>
-              </Link>
-              <p>{getText(post.desc)}</p>
-              <button>Read More</button>
-            </div>
+    <div className="table">
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Workout</th>
+          <th>Sets</th>
+          <th>Reps</th>
+        </tr>
+      </thead>
+      <tbody>
+        {workouts.map(workout =>
+         <tr>
+          <div className="align-me">
+          <td>{workout.name}</td>
+          <td>{workout.sets}</td>
+          <td>{workout.reps}</td>
           </div>
-        ))}
-      </div>
+        </tr>
+          )}    
+      </tbody>
+    </Table>
     </div>
-  );
-};
+    
+  )
 
-export default Home;
-//i dont think we need a login page because of the google auth
+}
+
+export default SW;
+ 
