97ba72d79a424c0e56f73e674f6a39a53b727bf7
Nivig1310 <n.garimella@ufl.edu>
2022-12-05 15:38:53 -0500

    saved page with backend connection

--- client/src/pages/Saved_Workouts.jsx
+++ client/src/pages/Saved_Workouts.jsx
@@ -1,54 +1,78 @@
 import React from "react";
-import {useContext, useState} from "react";
+import {useeffect, useState} from "react";
 import { useEffect } from "react";
 import Table from 'react-bootstrap/Table';
+import axios from "axios";
 import UserProvider from "../contexts/UserProvider";
+import {useContext} from "react";
 
 
-function SW(){
+const SW = () => {
   //array of workout objects
   const [workouts, setWorkouts] = useState([{
     name: '',
     sets: '',
-    reps: '' 
+    reps: ''
   }])
-  const userData = useContext(UserProvider.Context);
-  console.log(userData);
-  //fetches data from fetch link that is TBD
-  // useEffect(()=>{
-  //   fetch("/<TBD>").then(res=>{
-  //     if(res.ok){
-  //       return res.json()
-  //     }
-  //   }).then(jsonRes=>setWorkouts(jsonRes));
-  // })
+  const userData=useContext(UserProvider.Context);
+
+  //sends user info to database and waits for user-specific data to be returned
+  const sendRequest = async () => {
+    const res = await axios.get("http://localhost:5001/workout/"+userData.providerID)
+    const data = await res.data;
+    return data
+  }
+  useEffect(() => {
+    sendRequest().then((data)=>setWorkouts(data.workouts))
+  }, [])
+
+  console.log(workouts);
+
+  
+  const WorkoutGen =()=>{
+    //if workouts array is not empty, below code is returned and workouts are projected
+    if(workouts.length>1){
+      return(
+        <div className="saved">
+          <h1>Your Fitness Plan</h1>  
+        <Table striped bordered hover size = "sm">
+      <thead>
+          <tr>
+            <th>Workout</th>
+            <th>Sets</th>
+            <th>Reps</th>
+          </tr>
+        </thead>
+        <tbody>
+        {workouts.map(workout =>
+          <tr>
+           <td>{workout.name}</td>
+           <td>{workout.sets}</td>
+           <td>{workout.reps}</td>
+         </tr>
+           )}
+        </tbody>  
+      </Table>
+      </div>
+        )           
+
+    }
+    else{
+      //if workouts array is empty
+      return(
+        <div className="saved">
+        <h1>No workouts yet!</h1>
+      </div>
+      )
+
+    }
+
+  }
 
   return (
     <div>
-      <div className="saved">
-        <h1>Your Fitness Plan</h1>
-    <Table striped bordered hover size = "sm">
-    <thead>
-        <tr>
-          <th>Workout</th>
-          <th>Sets</th>
-          <th>Reps</th>
-        </tr>
-      </thead>
-      <tbody>
-        {/* {workouts.map(workout =>
-         <tr>
-          <td>{workout.name}</td>
-          <td>{workout.sets}</td>
-          <td>{workout.reps}</td>
-        </tr>
-          )}     */}
-      </tbody>  
-    </Table> 
-    </div>
-    </div>
-    
-    
+      <WorkoutGen/>
+    </div>   
   );
 
 }
