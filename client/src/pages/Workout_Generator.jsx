a5a89d6184507e032acef19a9a73eeca322be3a3
michelestew <mstewart2@ufl.edu>
2022-12-05 17:38:10 -0500

    fixed router to saved workouts

--- client/src/pages/Workout_Generator.jsx
+++ client/src/pages/Workout_Generator.jsx
@@ -1,21 +1,17 @@
-<<<<<<< HEAD
-import React from "react";
-import axios from 'axios';
-=======
 import React, { useContext, useState } from 'react';
 import UserProvider from "../contexts/UserProvider.jsx";
->>>>>>> 83fdbbf718fe28d2d7aa574e973a43a42d435128
 import { Dropdown, Option } from "./WorkoutGeneratorComponents/DropDown.js";
 import {
   FormWrapper,
   Wrapped,
   StyledButton,
 } from "./WorkoutGeneratorComponents/stylesDropDown.js";
-import AuthenticatedUser from '../components/AuthenticatedUser';
 import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
 
 const WG = () =>
 {
+  const navigate = useNavigate();
   const userData = useContext(UserProvider.Context);
   const [selectedExercises,setSelectedExercises] = useState("1");
   const [selectedDiff,setSelectedDiff] = useState("Easy");
@@ -45,16 +41,16 @@ const WG = () =>
       numExercises: selectedExercises
     };
     axios.post("http://localhost:5001/workout/" + userData.providerId, body);
+
+    //redirect to saved workouts page
+    navigate('/savedworkouts');
   }
 
   return (
 
     //found in sytle.scss
     <div className="img-background">
-        <FormWrapper
-          //action needs to be changed
-          action="/workoutgenerator"
-        >
+        <FormWrapper>
         <div>
           <h1>Create New Workout</h1>
           </div>
@@ -85,7 +81,6 @@ const WG = () =>
             </Dropdown>
           </Wrapped>
           <StyledButton type="submit" value="Generate Workout" onClick={handleSubmit}/>
-          <p>You selected {selectedExercises},{selectedDiff} </p>
         </FormWrapper>
     </div>
   )
