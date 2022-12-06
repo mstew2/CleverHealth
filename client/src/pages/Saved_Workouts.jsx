398776e6e1596af96350292b5ac57716619f7b4f
Nivig1310 <n.garimella@ufl.edu>
2022-12-06 09:12:33 -0500

    updated saved page

--- client/src/pages/Saved_Workouts.jsx
+++ client/src/pages/Saved_Workouts.jsx
@@ -3,53 +3,86 @@ import {useContext, useState} from "react";
 import { useEffect } from "react";
 import Table from 'react-bootstrap/Table';
 import UserProvider from "../contexts/UserProvider";
+import axios from "axios"
+import Carousel from 'react-bootstrap/Carousel';
 
 
-function SW(){
-  //array of workout objects
-  const [workouts, setWorkouts] = useState([{
-    name: '',
-    sets: '',
-    reps: '' 
-  }])
+const SW = () => {
+  
+  const [counter, setCounter] = useState(0);
+  const increase = () =>{
+    setCounter(count=>count+1);
+
+  };
+  //data variable holds all the workouts generated 
+  let data = []
   const userData = useContext(UserProvider.Context);
-  console.log(userData);
-  //fetches data from fetch link that is TBD
-  // useEffect(()=>{
-  //   fetch("/<TBD>").then(res=>{
-  //     if(res.ok){
-  //       return res.json()
-  //     }
-  //   }).then(jsonRes=>setWorkouts(jsonRes));
-  // })
+  if (userData) {
+    axios.get("http://localhost:5001/workout/get/" + userData.providerId).then(resp => {
+      console.log(resp);
+      data = resp.data
+    });
+  }
+  
+  /* The below code maps through the workouts and makes a table for each workout and stores it in a carousel */
+  let i = 1;
+  function workoutgen(){
+    if(userData.workouts != undefined){
+    return (
+      <div>
+        <div className="saved">
+          <h1>Your Fitness Plan</h1>
+          <Carousel>
+          {(userData.workouts).map(workout =>
+          <Carousel.Item>
+      <Table striped bordered hover size = "sm">
+      <thead>
+          <tr>
+            <th>Workout</th>
+            <th>Sets</th>
+            <th>Reps</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+          {workout.map(params => 
+          <tr>  
+           <td>{params.name}</td>
+           <td>{params.sets}</td>
+           <td>{params.reps}</td> 
+          </tr>
+             )}   
+        </tbody>  
+      </Table>
+      </Carousel.Item>
+      
+      )} 
+      </Carousel> 
+      </div>
+      </div>
+      
+      
+    );
+  }
+  else{
+    return(
+      <div>
+        <div className="saved">
+          <h1>No workouts yet!</h1>
+        </div>
+      </div>
+    )
+  }
+
+  }
+
+console.log(userData.workouts)
 
   return (
-    <div>
-      <div className="saved">
-        <h1>Your Fitness Plan</h1>
-    <Table striped bordered hover size = "sm">
-    <thead>
-        <tr>
-          <th>Workout</th>
-          <th>Sets</th>
-          <th>Reps</th>
-        </tr>
-      </thead>
-      <tbody>
-        {/* {workouts.map(workout =>
-         <tr>
-          <td>{workout.name}</td>
-          <td>{workout.sets}</td>
-          <td>{workout.reps}</td>
-        </tr>
-          )}     */}
-      </tbody>  
-    </Table> 
-    </div>
-    </div>
-    
-    
-  );
+    workoutgen()
+  )
+     
+  
 
 }
 
